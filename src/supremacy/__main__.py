adcf4e84c82f3b1d9c2f6a8f78f31e68445214b5
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-08-21 17:48:32 +0200

    Use dedicated Bot class and module

--- src/supremacy/__main__.py
+++ src/supremacy/__main__.py
@@ -1,19 +1,16 @@
 import argparse
-import importlib
-from functools import partial
-from importlib.resources import open_binary
 from pathlib import Path
 import tomllib
 
 import supremacy
+from supremacy.bots import load_bots
 
 
 def main():
     args = parse_arguments()
     config = load_config(args.config)
-    bot_configs = load_bot_configs(config)
-    bot_classes = load_bot_classes(bot_configs)
-    bot_factories = make_bot_factories(config, bot_classes, bot_configs)
+    bots = load_bots(config)
+    bot_factories = {bot.name: bot.factory() for bot in bots}
 
     cfg = config["supremacy"]
     supremacy.start(
@@ -37,46 +34,6 @@ def parse_arguments():
 def load_config(path):
     with open(path, "rb") as f:
         return tomllib.load(f)
-
-
-def collect_bot_packages(config):
-    packages = set(config["game"]["bots"])
-    if extra := config["game"].get("extra-bots"):
-        packages.add(extra["package"])
-    return packages
-
-
-def load_bot_config(package_name):
-    with open_binary(package_name, "config.toml") as file:
-        return tomllib.load(file)
-
-
-def load_bot_configs(config):
-    return {
-        package: load_bot_config(package) for package in collect_bot_packages(config)
-    }
-
-
-def load_bot_classes(bot_configs):
-    return {
-        package: getattr(importlib.import_module(package), cfg["class_name"])
-        for package, cfg in bot_configs.items()
-    }
-
-
-def make_bot_factories(config, bot_classes, bot_configs):
-    base = {
-        bot_configs[package]["name"]: partial(
-            bot_classes[package], team=bot_configs[package]["name"]
-        )
-        for package in config["game"]["bots"]
-    }
-    extra_config = config["game"]["extra-bots"]
-    extra = {
-        f"Bot{i}": partial(bot_classes[extra_config["package"]], team=f"Bot{i}")
-        for i in range(extra_config["n"])
-    }
-    return {**base, **extra}
 
 
 if __name__ == "__main__":
